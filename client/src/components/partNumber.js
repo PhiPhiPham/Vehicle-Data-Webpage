eefb0d0fe26a61093dbfea66e1069e7eaefdf8c5
PhiPhiPham <Stevenpham0705@gmail.com>
2024-06-24 03:26:36 +1000

    display part number component added

---
+++ client/src/components/partNumber.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PartNumber = ({ partNumber }) => {
+  return (
+    <div>
+      <p>Part Number: {partNumber}</p>
+    </div>
+  );
+};
+
+export default PartNumber;
