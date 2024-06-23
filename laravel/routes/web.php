0297c7f7b7cad0e9283d38a27b9c0497297f0255
PhiPhiPham <Stevenpham0705@gmail.com>
2024-06-23 20:41:42 +1000

    redid laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
+    return view('welcome');
 });
