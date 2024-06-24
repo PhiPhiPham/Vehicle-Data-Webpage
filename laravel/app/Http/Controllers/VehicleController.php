e579910ae9feeb82b670a5bc4956e26f8aee75c8
PhiPhiPham <Stevenpham0705@gmail.com>
2024-06-24 18:29:00 +1000

    Added the routes and controller for API

--- laravel/app/Http/Controllers/VehicleController.php
+++ laravel/app/Http/Controllers/VehicleController.php
@@ -23,5 +23,42 @@ class VehicleController extends Controller
         $vehicle= Vehicle::where('make', 'AIRMAN')->first();
         return $vehicle;
     }
+
+    // Get distinct makes
+    public function getMakes()
+    {
+        $makes = Vehicle::distinct()->pluck('make');
+        return response()->json($makes);
+    }
+
+    // Get distinct models for a given make
+    public function getModels($make)
+    {
+        $models = Vehicle::where('make', $make)
+                        ->distinct()
+                        ->pluck('model');
+        return response()->json($models);
+    }
+
+    // Get distinct types for a given make and model
+    public function getTypes($make, $model)
+    {
+        $types = Vehicle::where('make', $make)
+                        ->where('model', $model)
+                        ->distinct()
+                        ->pluck('type');
+        return response()->json($types);
+    }
+
+    // Get part number for a given make, model, and type
+    public function getPartNumber($make, $model, $type)
+    {
+        $partNumber = Vehicle::where('make', $make)
+                             ->where('model', $model)
+                             ->where('type', $type)
+                             ->pluck('part_number')
+                             ->first();
+        return response()->json(['part_number' => $partNumber]);
+    }
    
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -8,5 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('register',[VehicleController::class, 'register']);
-Route::post('login',[VehicleController::class, 'login']);
+Route::post('/register',[VehicleController::class, 'register']);
+Route::post('/login',[VehicleController::class, 'login']);
+
+Route::get('/makes', [VehicleController::class, 'getMakes']);
+Route::get('/models/{make}', [VehicleController::class, 'getModels']);
+Route::get('/types/{make}/{model}', [VehicleController::class, 'getTypes']);
+Route::get('/part-number/{make}/{model}/{type}', [VehicleController::class, 'getPartNumber']);
