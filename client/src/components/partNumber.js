78cf178cfa5ca1036bd650b159e48a0ab456980d
PhiPhiPham <Stevenpham0705@gmail.com>
2024-06-26 03:10:58 +1000

    Added dark mode to partNumber.js

--- client/src/components/partNumber.js
+++ client/src/components/partNumber.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PartNumber = ({ partNumber }) => {
   return (
-    <div className="mt-6 p-4 bg-orange-100 border border-orange-200 rounded">
-      <p className="text-orange-800 font-bold">Part Number: {partNumber}</p>
+    <div className="mt-6 p-4 bg-orange-100 dark:bg-slate-100 border dark:border-slate-200 border-orange-200 rounded">
+      <p className="text-orange-800 dark:text-gray-700 font-bold">Part Number: {partNumber}</p>
     </div>
   );
 };
