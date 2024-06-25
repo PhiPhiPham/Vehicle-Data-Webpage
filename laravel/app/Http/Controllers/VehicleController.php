11fca0ca7648ebc9854520930502616166e93993
PhiPhiPham <Stevenpham0705@gmail.com>
2024-06-26 01:21:35 +1000

    Got rid of unnecessary code for API.

--- laravel/app/Http/Controllers/VehicleController.php
+++ laravel/app/Http/Controllers/VehicleController.php
@@ -7,23 +7,6 @@ use App\Models\Vehicle;
 
 class VehicleController extends Controller
 {
-    //
-    function register(Request $req) 
-    {
-        $vehicle= new Vehicle;
-        $vehicle->make= $req->input('make');
-        $vehicle->model= $req->input('model');
-        $vehicle->type= $req->input('type');
-        $vehicle->part_number= $req->input('part_number');
-        return $vehicle;
-    }
-
-    function login(Request $req)
-    {
-        $vehicle= Vehicle::where('make', 'AIRMAN')->first();
-        return $vehicle;
-    }
-
     // Get distinct makes
     public function getMakes()
     {
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,13 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VehicleController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/register',[VehicleController::class, 'register']);
-Route::post('/login',[VehicleController::class, 'login']);
-
 Route::get('/makes', [VehicleController::class, 'getMakes']);
 Route::get('/models/{make}', [VehicleController::class, 'getModels']);
 Route::get('/types/{make}/{model}', [VehicleController::class, 'getTypes']);
